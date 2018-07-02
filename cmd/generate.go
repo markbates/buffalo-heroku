3c53e9f7883d1d25080fad3bedc427d32b199bcb
Mark Bates <mark@markbates.com>
2018-07-02 11:39:59 -0400

    updated to use heroku.yml

--- cmd/generate.go
+++ cmd/generate.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"bytes"
+	"io/ioutil"
 	"os"
 
 	"github.com/pkg/errors"
@@ -12,17 +14,34 @@ var generateCmd = &cobra.Command{
 	Aliases: []string{"h"},
 	Short:   "generates a heroku.yml file for deploying a buffalo app to heroku with docker",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// return deployContainer()
-		f, err := os.Create("heroku.yml")
-		if err != nil {
-			return errors.WithStack(err)
-		}
-		_, err = f.WriteString(yml)
-		if err != nil {
-			return errors.WithStack(err)
-		}
-		return nil
+		return WriteHerokuYml()
 	},
+}
+
+func WriteHerokuYml() error {
+	f, err := os.Create("heroku.yml")
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	_, err = f.WriteString(yml)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	b, err := ioutil.ReadFile("Dockerfile")
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	if bytes.Contains(b, []byte("RUN apk add --no-cache curl")) {
+		return nil
+	}
+	b = bytes.Replace(b, []byte("FROM alpine\n"), []byte("FROM alpine\nRUN apk add --no-cache curl"), 1)
+	f, err = os.Create("Dockerfile")
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	defer f.Close()
+	_, err = f.Write(b)
+	return err
 }
 
 const yml = `build:
